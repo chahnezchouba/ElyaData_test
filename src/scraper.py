8d4bba7faac8f72202512327ad3c4cde6160b589
chahnezchouba <75624114+chahnezchouba@users.noreply.github.com>
2023-09-09 22:58:08 +0200

    update scraper file

--- src/scraper.py
+++ src/scraper.py
@@ -1,17 +1,22 @@
 from facebook_scraper import get_posts
 from contextlib import suppress
 
+
 def scrape(page_name, page_limit):
-    email = 'testscraping65@gmail.com'
-    password = 'testtest789t'
-    with suppress(Exception) :
-        page_data=[]
-        for post in get_posts(page_name, pages=page_limit, credentials=(email, password)):
-            page_data.append({'time' : (post['time']),
-                              'text' : post['text'][:200].isoformat(),
+    email = 'chaimascrap@gmail.com'
+    password = 'chaima789/'
+    page_data=[]
+    
+    for post in get_posts(page_name, pages=page_limit, credentials=(email, password)):
+        with suppress(Exception) :
+            page_data.append({'page_name' : page_name,
+                              'time' : (post['time'].isoformat()),
+                              'text' : post['text'][:200],
                               'image' : post['image'][:200],
                               'likes' : post['likes'],
                               'comments' : post['comments'],
-                              'shares' : post['shares'],
-                            })
+                              'shares' : post['shares']
+                              })
+
     return page_data
+
