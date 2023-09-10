1abeb6c2e372ffabdbee80e6d7e83f3641f56b7b
chahnezchouba <75624114+chahnezchouba@users.noreply.github.com>
2023-09-10 22:25:49 +0200

    update scrape function

--- src/scraper.py
+++ src/scraper.py
@@ -1,4 +1,4 @@
-from facebook_scraper import get_posts
+import facebook_scraper
 from contextlib import suppress
 
 
@@ -6,8 +6,10 @@ def scrape(page_name, page_limit):
     email = 'chaimascrap@gmail.com'
     password = 'chaima789/'
     page_data=[]
-    
-    for post in get_posts(page_name, pages=page_limit, credentials=(email, password)):
+    posts = facebook_scraper.get_posts(page_name, 
+                                       pages=page_limit, 
+                                       credentials=(email, password))
+    for post in posts:
         with suppress(Exception) :
             page_data.append({'page_name' : page_name,
                               'time' : (post['time']),
