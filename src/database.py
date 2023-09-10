b0c3f9b076403218772aaaa74751b98f22db66c2
chahnezchouba <75624114+chahnezchouba@users.noreply.github.com>
2023-09-10 17:17:25 +0200

    correct database url

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:chahnez@localhost:5432/testdb"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test789@scrap-db:5432/fbscrap"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
